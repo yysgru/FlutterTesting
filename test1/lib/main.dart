1cc2183147fa7d38cef3f37155505df76d9d4c52
yysgru <anamonterorod.paradas@gmail.com>
2026-02-22 14:00:18 +0100

    Overall understanding of the style widgets such as buttons, text style and icons.

--- test1/lib/main.dart
+++ test1/lib/main.dart
@@ -14,20 +14,84 @@ void main() {
     MaterialApp(
       //especifica QUÉ es lo que va a estar en la home screen cuando
       //se inicia la apliación
-      home: Scaffold(
-        //propiedad de barra superior que mostrará el widget de esta
-        appBar: AppBar(title: Text('My first test'), centerTitle: true),
-        //especifica qué es lo que irá debajo de la barra superior
-        body: Center(
-          //centraliza todo lo que está metido dentro
-          //se pone child porque es el HIJO de un WIDGET
-          child: Text("Hello world"),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Text("Click"),
-        ),
-      ),
+      home: Home(),
     ),
   );
+}
+
+//stateless widget: el estado del widget NO PUEDE CAMBIAR mientras usamos la app
+//stateful widget: pueden cambiar mientras usamos la app
+
+// ignore: slash_for_doc_comments
+/**
+ * Con este stateless widget se puede hacer un hot reload simplemente
+ * guardando el fichero, pulsando r y no es necesario volver a recargar.
+ * También se podrá usar este widget en varias partes si así lo queremos.
+ * Serviría para las tabs por ejemplo.
+ */
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  //lo que indica la etiqueta "override" es que la clase que definamos sustituirá a la
+  //funcion build que se hereda de StatelessWidget
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //propiedad de barra superior que mostrará el widget de esta
+      appBar: AppBar(
+        title: Text('My first test'),
+        centerTitle: true,
+        //colores del material design, si quieres un color en específico
+        //tienes que buscarlo y poner [x] después del nombre del color
+        backgroundColor: Colors.deepOrange,
+      ),
+      //especifica qué es lo que irá debajo de la barra superior
+      body: Center(
+        //centraliza todo lo que está metido dentro
+        //se pone child porque es el HIJO de un WIDGET
+        child:
+            //Image.asset("assets/descarga.jfif"),
+            /*Icon(
+              Icons.airport_shuttle,
+              color: const Color.fromARGB(255, 74, 162, 255),
+              size: 100.0,
+            ),*/
+            //se usa child cuando se anida un widget dentro de otro widget
+            /*ElevatedButton.icon(
+              onPressed: () {
+                //print("You just clicked me");
+              },
+              icon: Icon(Icons.mail, color: Colors.white),
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+              label: Text(
+                "Botoncito",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              /*child: Text(
+                "Botoncito",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: const Color.fromARGB(255, 255, 255, 255),
+                ),
+              ),*/
+            ),*/
+            IconButton(
+              //para tabs
+              onPressed: () {},
+              icon: Icon(Icons.alternate_email),
+              color: Colors.blue,
+            ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        //función anónima de momento
+        onPressed: () {},
+        backgroundColor: Colors.deepOrange,
+        //el hijo se suele poner al final
+        child: Text("Click me"),
+      ),
+    );
+  }
 }
