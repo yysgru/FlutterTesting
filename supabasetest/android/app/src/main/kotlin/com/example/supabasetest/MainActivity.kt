1ea619a12921ce76f85e7ac633851116ac088a9e
yysgru <anamonterorod.paradas@gmail.com>
2026-02-25 13:07:29 +0100

    Project connected to SupaBase

---
+++ supabasetest/android/app/src/main/kotlin/com/example/supabasetest/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.supabasetest
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ supabasetest/lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://gcgcfdbttnprtqbdlpwn.supabase.co',
+    anonKey: 'sb_publishable_paxXADz78_HGwrWoxEVXjQ_BcDLQsBD',
+  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(title: 'Products', home: HomePage());
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _future = Supabase.instance.client.from('Products').select();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FutureBuilder(
+        future: _future,
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          final instruments = snapshot.data!;
+          return ListView.builder(
+            itemCount: instruments.length,
+            itemBuilder: ((context, index) {
+              final instrument = instruments[index];
+              return ListTile(
+                title: Text(instrument['name']?.toString() ?? 'Sin nombre'),
+              );
+            }),
+          );
+        },
+      ),
+    );
+  }
+}
